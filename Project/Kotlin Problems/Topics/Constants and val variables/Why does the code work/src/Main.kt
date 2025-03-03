72935de43f909e3cc748df1db35eaaef946131d8
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-03-03 21:16:11 +0100

    removed files which is not related to project

--- Project/Kotlin Problems/Topics/Constants and val variables/Why does the code work/src/Main.kt
+++
@@ -1,8 +0,0 @@
-// You can experiment here, it won’t be checked
-
-fun main(args: Array<String>) {
-  // put your code here
-    val myList = mutableListOf(1, 2, 3, 4, 5)
-    myList.remove(3)
-    println(myList)
-}
